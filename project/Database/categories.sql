29a7c0b3561b0ccf80ab025fb93e799f1281a5ad
basamakis <75481484+basamakis@users.noreply.github.com>
2021-06-07 17:24:25 +0300

    Delete project directory

--- project/Database/categories.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `category` (`topic`) VALUES ('Biology');
-INSERT INTO `category` (`topic`) VALUES ('Economics');
-INSERT INTO `category` (`topic`) VALUES ('Engineering');
-INSERT INTO `category` (`topic`) VALUES ('History');
-INSERT INTO `category` (`topic`) VALUES('Language');
-INSERT INTO `category` (`topic`) VALUES ('Mathematics');
-INSERT INTO `category` (`topic`) VALUES ('Politics');
-INSERT INTO `category` (`topic`) VALUES ('Sience');
-INSERT INTO `category` (`topic`) VALUES ('Sociology');
